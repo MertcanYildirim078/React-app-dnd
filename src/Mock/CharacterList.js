23dbf3d526d44769a26bccbe51584ebe39a889f1
MertcanYildirim078 <99068314@mydavinci.nl>
2023-01-04 15:46:53 +0100

    'WVR-SDK'

--- src/Mock/CharacterList.js
+++ src/Mock/CharacterList.js
@@ -1,40 +1,41 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import Weaver from 'weaver-sdk';
 
 function Characterlist() {
+
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      const makeConnection = async () => {
+        var weaver = undefined;
+        try {
+            weaver = Weaver.getInstance();
+        } catch(e) {
+            weaver = new Weaver();
+            await weaver.connect('https://demo.weaverplatform.com/');
+            await weaver.signInWithUsername('myildirim', '2it2M3m*UGup');
+            await weaver.useProject('dnd');
+        }
+        var nodes = await new Weaver.Query().hasRelationOut('type', 'character').find();
+        setCharacters(nodes);
+      }
+      makeConnection();
+      return undefined;
+    }, [])
+
     const style = {
         color: 'red'
     }
     return(
         <div className="CharacterContainer">
-            <Link to={'Argus'} style = {style}><div className="Listitem">Argus (Assassin)</div></Link>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
-            <div className="Listitem">A Character</div>
+            {
+            characters.map((item) => 
+                <Link to={'Argus'} style = {style}>
+                    <div className="Listitem">{item.get('name')}</div>
+                </Link>
+            )
+            }
         </div> 
     )
 }
